c613ce02fc49ac490845500582817abdda929324
Lokesh Gurung <lokeshgurungwj@gmail.com>
2022-02-12 16:52:59 +0545

    Reogranize the folder structure

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Okay');
+});
+
+app.listen(port, () => {
+  console.log(`The server is running at port ${port}`);
+});
